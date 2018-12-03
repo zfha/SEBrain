310ce3b1e2a4d1ac4afb5c599745dfb5b9c8d2a5
zhoufan <zhoufan@qingguatang.com>
2018-12-03 21:41:35 +0800

    :sparkles: (加入tab的功能)

--- app/components/Main/index.js
+++ app/components/Main/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Editor from 'draft-js-plugins-editor';
 import createMarkdownShortcutsPlugin from 'draft-js-markdown-shortcuts-plugin';
-import { EditorState } from 'draft-js';
+import { EditorState, Modifier } from 'draft-js';
 import Prism from 'prismjs';
 import createPrismPlugin from 'draft-js-prism-plugin';
 import * as css from './style';
@@ -12,6 +12,11 @@ const prismPlugin = createPrismPlugin({
 const plugins = [prismPlugin, createMarkdownShortcutsPlugin()];
 
 export default class MainEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.handleKeyEvent = this.handleKeyEvent.bind(this);
+  }
+
   state = {
     editorState: EditorState.createEmpty()
   };
@@ -23,9 +28,28 @@ export default class MainEditor extends Component {
     });
   };
 
+  _insertText(appendContent) {
+    const editorState = this.state.editorState;
+    const selection = editorState.getSelection();
+    const contentState = editorState.getCurrentContent();
+    const ncs = Modifier.insertText(contentState, selection, appendContent);
+    const es = EditorState.push(editorState, ncs, 'insert-fragment');
+    this.setState({
+      editorState: es
+    });
+  }
+
+  handleKeyEvent = e => {
+    if (e.keyCode === 9) {
+      // 插入tab制表符
+      e.preventDefault();
+      this._insertText('    ');
+    }
+  };
+
   render() {
     return (
-      <css.MainStyle>
+      <css.MainStyle onKeyDown={this.handleKeyEvent}>
         <Editor
           style={{ height: '100%' }}
           editorState={this.state.editorState}
