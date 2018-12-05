d8c8a002df286a73a4fc39757129ae76bba14d13
zhoufan <zhoufan@qingguatang.com>
2018-12-05 20:28:09 +0800

    :sparkles: (加入dva)

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,24 +1,69 @@
-// @flow
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
-import type { Store } from '../reducers/types';
-import Routes from '../Routes';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { connect, Provider } from 'react-redux';
+import { create } from 'dva-core';
+import homeModel from '../model/home';
+import view from './HomePage';
 
-type Props = {
-  store: Store,
-  history: {}
-};
+export default wrapper({ model: homeModel, view, selector: v => v.page });
 
-export default class Root extends Component<Props> {
-  render() {
-    const { store, history } = this.props;
-    return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Routes />
-        </ConnectedRouter>
-      </Provider>
-    );
+function wrapper({ model, models, view, selector, mount, initialState }) {
+  const root = mount && getRoot(mount);
+  initialState = initialState || (root ? getState(root) : null);
+  const app = create({ initialState });
+  models = toArray(model || models);
+  models.forEach(v => app.model(v));
+  app.start();
+  const store = app._store; // eslint-disable-line
+  const View = withMount({ selector, models })(view);
+
+  const App = props => (
+    <Provider store={store}>
+      <View {...props} />
+    </Provider>
+  );
+
+  return App;
+}
+
+function toArray(array) {
+  return Array.isArray(array) ? array : [array];
+}
+
+function withMount({ selector, models }) {
+  selector = selector || (v => v);
+  return View => {
+    class App extends React.Component {
+      componentDidMount() {
+        const { props } = this;
+        models.forEach(({ namespace }) => {
+          props.dispatch({ type: `${namespace}/mount`, ...props });
+        });
+      }
+
+      render() {
+        return <View {...this.props} />;
+      }
+    }
+    return connect(selector)(App);
+  };
+}
+
+function render(App, root) {
+  ReactDOM.render(<App />, root);
+}
+
+function getRoot(mount) {
+  if (typeof mount === 'string') {
+    return document.querySelector(mount);
   }
+  if (mount && mount.nodeType === 1) {
+    return mount;
+  }
+  return document.getElementById('app');
+}
+
+function getState(root) {
+  const state = root.dataset.state;
+  return state ? JSON.parse(state) : null;
 }
---
+++ app/model/home.js
@@ -0,0 +1,8 @@
+export default {
+  namespace: 'page',
+  state: {},
+
+  effects: {
+    mount() {}
+  }
+};
